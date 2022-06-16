4a6aebe8b272a679844ca90652751573eedbfc36
Sergey <Sergeev_sergey99@mail.ru>
2022-06-16 13:43:44 +0300

    35

---
+++ Stepik_Lesson35/app/src/main/java/com/example/company/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.company.myapplication
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.CAMERA),
+                0
+            )
+        }
+    }
+}
